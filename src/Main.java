83aab48673b4f40dbc0a3c45d224d22253879c3a
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-01-09 18:48:58 +0700

    update project

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        String str = "Hello, World";
+        String str = "Hello, World (Привет) 777";
 
         int count_lowercase  = 0;
         int count_uppercase = 0;
 
+
         for (int i = 0; i < str.length(); i++) {
-            if (Character.isLowerCase(str.charAt(i))) {
-                count_lowercase++;
-            }
-            if (Character.isUpperCase(str.charAt(i))) {
-                count_uppercase++;
+            char ch = str.charAt(i);
+            if (String.valueOf(ch).matches("[a-zA-Z]")){
+                if (Character.isLowerCase(str.charAt(i))) {
+                    count_lowercase++;
+                }
+                if (Character.isUpperCase(str.charAt(i))) {
+                    count_uppercase++;
+                }
             }
         }
 
         System.out.println("Дана строка - " + str);
         System.out.print("Количество строчных букв - " + count_lowercase + ", количество прописных букв - " + count_uppercase);
         System.out.println();
-
     }
 }
